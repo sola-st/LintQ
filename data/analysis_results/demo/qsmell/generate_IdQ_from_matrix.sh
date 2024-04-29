5cb9acd053c885f643d2caf1c87dcfc602748564
mattepalte <mattepalte@live.it>
2024-04-29 22:20:11 +0200

    clean demo in analysis results

--- data/analysis_results/demo/qsmell/generate_IdQ_from_matrix.sh
+++
@@ -1,5 +0,0 @@
-for RELATIVE_FILEPATH in matrix_format/*
-do
-    FILE_NAME=$(basename $RELATIVE_FILEPATH)
-    python -m qsmell --smell-metric IdQ --input-file $RELATIVE_FILEPATH --output-file metrics/IdQ/$FILE_NAME
-done
